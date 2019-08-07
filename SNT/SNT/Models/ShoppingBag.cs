e13fa072e12a5171606d3a6d20f157bc0ec01b87
danailstratirv <stratyd@abv.bg>
2019-08-07 10:29:20 +0300

    Added base models

---
+++ SNT/SNT/Models/ShoppingBag.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SNT.Models
+{
+    public class ShoppingBag
+    {
+        public List<Tyre> Tyres { get; set; }
+
+        public decimal TotalPrice { get; set; }
+    }
+}
---
+++ SNT/SNT/Models/Tyre.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SNT.Models
+{
+    public class Tyre
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Brand { get; set; }
+
+        public string Type { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int Width { get; set; }
+
+        public int Ratio { get; set; }
+
+        public int Diameter { get; set; }
+    }
+}
