69e16697b91a382d3c96ea20e3c5c89121cc57e8
danailstratirv <stratyd@abv.bg>
2019-08-22 21:51:52 +0300

    Added WheelRim view, service and insput models

--- SNT/SNT/ViewModels/WheelRimViewModel.cs
+++ SNT/SNT/ViewModels/WheelRimViewModel.cs
@@ -1,13 +1,12 @@
 ﻿using SNT.Models.Enums;
-using SNT.Models.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace SNT.Models
+namespace SNT.ViewModels
 {
-    public class WheelRim : IProduct
+    public class WheelRimViewModel
     {
         public string Id { get; set; }
 
@@ -16,19 +15,15 @@ namespace SNT.Models
         public string Brand { get; set; }
 
         public AvailabilityStatus Status { get; set; }
-                
+
         public decimal Price { get; set; }
 
         public string Picture { get; set; }
 
-
-        //Междуболтово разстояние (PCD)
         public string PCD { get; set; }
 
-        //Централен отвор
         public double CentralLukeDiameter { get; set; }
 
-        //Офсет (ЕТ)
         public int Offset { get; set; }
 
         public string Material { get; set; }
