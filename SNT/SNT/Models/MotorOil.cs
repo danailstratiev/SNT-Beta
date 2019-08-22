6aa1d2d0e130f2aee8e8e2369dc380c3449f8b85
danailstratirv <stratyd@abv.bg>
2019-08-22 20:27:48 +0300

    MotorOil entity added

---
+++ SNT/SNT/Models/MotorOil.cs
@@ -0,0 +1,28 @@
+﻿using SNT.Models.Enums;
+using SNT.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SNT.Models
+{
+    public class MotorOil : IProduct
+    {
+        public string Model { get; set; }
+
+        public string Brand { get; set; }
+
+        public decimal Price { get; set; }      
+
+        public string Viscosity { get; set; }
+
+        public int Volume { get; set; }
+
+        public string Type { get; set; }
+
+        public AvailabilityStatus Status { get; set; }
+
+        public string Description { get; set; }
+    }
+}
