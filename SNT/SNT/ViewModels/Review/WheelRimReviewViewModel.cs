e9b2c235fffe4f833cd1a2e8bbc47ad028847217
danailstratirv <stratyd@abv.bg>
2019-08-25 16:26:40 +0300

    Added WheelRim Review

---
+++ SNT/SNT/ViewModels/Review/WheelRimReviewViewModel.cs
@@ -0,0 +1,37 @@
+﻿using SNT.Models.Enums;
+using SNT.ServiceModels;
+using SNT.Services.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SNT.ViewModels.Review
+{
+    public class WheelRimReviewViewModel : IMapFrom<WheelRimServiceModel>
+    {
+        public string Id { get; set; }
+
+        public string Model { get; set; }
+
+        public string Brand { get; set; }
+
+        public AvailabilityStatus Status { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string Picture { get; set; }
+
+        public string PCD { get; set; }
+
+        public double CentralLukeDiameter { get; set; }
+
+        public int Offset { get; set; }
+
+        public string Material { get; set; }
+
+        public int YearOfProduction { get; set; }
+
+        public string Description { get; set; }
+    }
+}
