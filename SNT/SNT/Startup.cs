d6b89f54a2926e4784ec9d30743bbadfa2e73ed8
danailstratirv <stratyd@abv.bg>
2019-08-31 21:17:22 +0300

    Improving security

--- SNT/SNT/Startup.cs
+++ SNT/SNT/Startup.cs
@@ -63,11 +63,11 @@ namespace SNT
             services.Configure<IdentityOptions>(options =>
             {
                 // Password settings.
-                options.Password.RequireDigit = false;
-                options.Password.RequireLowercase = false;
-                options.Password.RequireNonAlphanumeric = false;
-                options.Password.RequireUppercase = false;
-                options.Password.RequiredLength = 3;
+                options.Password.RequireDigit = true;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireNonAlphanumeric = true;
+                options.Password.RequireUppercase = true;
+                options.Password.RequiredLength = 8;
                 options.Password.RequiredUniqueChars = 0;
 
                 options.User.RequireUniqueEmail = true;
